08f410d3686ecde0a2c01ac964c98fd02e72375f
Joe Brew <joebrew@gmail.com>
2020-03-14 01:26:05 +0100

    minor changes

--- misc/esp.R
+++ misc/esp.R
@@ -4,20 +4,20 @@ library(lubridate)
 library(dplyr)
 Sys.timezone()
 
-date_vec <- as.POSIXct(seq(as.Date('2020-03-02'),
+date_vec <- (seq(as.Date('2020-03-02'),
                            as.Date('2020-03-12'),
                            by = 2))
 date_lab <- format(date_vec, '%b\n%d')
 
 
-esp = covid19::esp %>% filter(!ccaa %in% 'Melilla') %>%
+esp = covid19::esp %>% # %>% filter(!ccaa %in% 'Melilla') %>%
   mutate(date = as.Date(date_time)) %>%
   group_by(ccaa, date) %>%
   filter(date_time == max(date_time)) %>%
   ungroup %>%
   dplyr::select(-date_time)
 ggplot(data = esp,
-       aes(x = date_time,
+       aes(x = date,
            y = value,
            group = ccaa)) +
   geom_line(color = 'black') +
@@ -25,17 +25,17 @@ ggplot(data = esp,
   theme_simple() + 
   scale_y_log10() +
   labs(x = '', y = 'Casos') +
-  geom_text(data = esp %>% filter(date_time == max(date_time)),
+  geom_text(data = esp %>% filter(date == max(date)),
             aes(label = value,
-                x = date_time - hours(50),
+                x = date - 2,
                 y = value + 150),
             size = 5,
             alpha = 0.6) +
   labs(title = 'Casos confirmados por CCAA',
-       subtitle = 'Datos: 17:00 12 de marzo 2020') +
+       subtitle = 'Datos: 18:00 13 de marzo 2020') +
   # ylim(0, 1600) +
   theme(strip.text = element_text(size = 15)) +
-  scale_x_datetime(breaks = date_vec,
+  scale_x_date(breaks = date_vec,
                    labels = date_lab)
 
 
